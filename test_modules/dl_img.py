c64d71c085d6bee3c699637cb95e2bcf958125bf
byrEE <paul13011307@gmail.com>
2015-10-01 00:20:05 +0800

    small changes about dl_img.py

--- test_modules/dl_img.py
+++ test_modules/dl_img.py
@@ -2,7 +2,9 @@
 # coding=utf-8
 from urllib import request
 import re
-
+#from PIL import Image
+#from io import BytesIO
+#import io
 start_url='http://image.hnol.net/c/2015-09/23/11/201509231128463691-2285289.jpg'
 dl_pattern=re.compile(r'.*?/\w/.*?/\d+/\d+/(\d+-\d+.jpg)')
 def dl_img(url):
@@ -11,6 +13,9 @@ def dl_img(url):
     #print(filename)
     with request.urlopen(url) as f:
         data=f.read()
+        #tmpIm=BytesIO(data)
+        #im=Image.open(tmpIm)
+        #print(im.size)
         with open(m,'wb') as img:
             img.write(data)
 
